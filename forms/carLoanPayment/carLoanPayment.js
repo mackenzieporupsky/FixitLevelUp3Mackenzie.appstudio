48dd39b5b650cc7a8ac9c8376e8344f222e160ab
mackenzieporupsky <mack@porupsky.net>
2020-10-04 22:51:08 -0500

    work
    code runs

--- forms/carLoanPayment/carLoanPayment.js
+++ forms/carLoanPayment/carLoanPayment.js
@@ -1,14 +1,27 @@
 
+
 btnBackHomeLoan.onclick=function(){
   ChangeForm(homePage)
 }
 
 
-function carLoan (p, r, n) {
-  return Number(p*(r*((1+ r)**n))/(((1+r)**n)-1))
+function carLoan () {
+  let p = inptCarLoan.value
+  let r = inptCarInterest.value
+  let n = inptCarTime.value
+  let payments = Number(p*(r*((1+ r)**n))/(((1+r)**n)-1))
+  let monthly = payments / 12
+  return monthly
  }
  
  
 btnCarSubmit.onclick=function(){
-lblAnswer.value = "A car loan for $" + inptCarLoan.value + " over " + inptCarTime.value + " years at " + inptCarInterest.value + "% interest would have monthly payment of $" + carLoan(Number(inptCarLoan.value, inptCarInterest.value, inptCarTime.value))
+lblAnswer.value = "A car loan for $" + inptCarLoan.value + " over " + inptCarTime.value + " years at " + inptCarInterest.value * 100 + "% interest would have monthly payment of $" + carLoan()
+}
+
+carReset.onclick=function(){
+  inptCarLoan.value = " "
+  inptCarInterest.value = " "
+  inptCarTime.value = " "
+  lblAnswer.value = " "
 }
--- forms/homeLoanPayment/homeLoanPayment.js
+++ forms/homeLoanPayment/homeLoanPayment.js
@@ -2,3 +2,29 @@
 btnBackHomeLoan.onclick=function(){
   ChangeForm(homePage)
 }
+
+
+
+
+function homeLoan () {
+  let p = inptHomeLoan.value
+  let r = inptHomeInterest.value
+  let n = inptHomeTime.value
+  let payments = Number(p*(r*((1+ r)**n))/(((1+r)**n)-1))
+  let monthly = payments / 12
+  return monthly
+ }
+ 
+
+ 
+Button1.onclick=function(){
+lblHomeAnswer.value = "A home loan for $" + inptHomeLoan.value + " over " + inptHomeTime.value + " years at " + inptHomeInterest.value *100 + "% interest would have monthly payment of $" + homeLoan()
+}
+
+
+homeReset.onclick=function(){
+  inptHomeLoan.value = " "
+  inptHomeInterest.value = " "
+  inptHomeTime.value = " "
+  lblHomeAnswer.value = " "
+}
